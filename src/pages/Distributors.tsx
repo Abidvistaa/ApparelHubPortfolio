bdab32f5ab014d80631dadcb6e2a80dd5903acc9
Abid Hassan <abidhassandiba@gmail.com>
2025-10-12 13:32:30 +0600

    Distributor Done & Final release

--- src/pages/Distributors.tsx
+++ src/pages/Distributors.tsx
@@ -3,10 +3,35 @@ import "./distributors.css";
 
 const Distributros: React.FC = () => {
   return (
-    <div className="distributors-container" style={{ padding: "5px" }}>
-      <h1>Distributros</h1>
-      <p>This is the Distributros.</p>
+    <div className="distributors-container">
+  <h1>Distributors</h1>
+    <div className="cardContainer">
+    <div className="card">
+      <h3>Guatemala</h3>
+      <p>Capital: Guatemala City</p>
     </div>
+
+    <div className="card">
+      <h3>Panama</h3>
+      <p>Capital: Panama City</p>
+    </div>
+
+    <div className="card">
+      <h3>Honduras</h3>
+      <p>Capital: Tegucigalpa</p>
+    </div>
+
+    <div className="card">
+      <h3>El Salvador</h3>
+      <p>Capital: San Salvador</p>
+    </div>
+
+    <div className="card">
+      <h3>Costa Rica</h3>
+      <p>Capital: San José</p>
+    </div>
+  </div>
+</div>
   );
 };
 
